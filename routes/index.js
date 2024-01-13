94172c2bf1b18a63c74c76e0307dc5899e400767
Diego Freile Garcia <dfreile51@gmail.com>
2024-01-13 12:18:08 +0100

    Proyecto finalizado

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,24 @@
 import express from "express";
+import {
+    paginaInicio,
+    paginaNosotros,
+    paginaTestimoniales,
+    paginaViajes,
+    paginaDetalleViaje,
+} from "../controllers/paginasController.js";
+import { guardarTestimonial } from "../controllers/testimonialController.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    // req - lo que enviamos : res - lo que express nos responde
-    res.render("inicio", {
-        pagina: "Inicio",
-    });
-});
-router.get("/nosotros", (req, res) => {
-    res.render("nosotros", {
-        pagina: "Nosotros",
-    });
-});
-router.get("/viajes", (req, res) => {
-    res.render("viajes", {
-        pagina: "Viajes",
-    });
-});
-router.get("/testimoniales", (req, res) => {
-    res.render("testimoniales", {
-        pagina: "Testimoniales",
-    });
-});
+router.get("/", paginaInicio);
+
+router.get("/nosotros", paginaNosotros);
+
+router.get("/viajes", paginaViajes);
+
+router.get("/viajes/:slug", paginaDetalleViaje);
+
+router.get("/testimoniales", paginaTestimoniales);
+router.post("/testimoniales", guardarTestimonial);
 
 export default router;
