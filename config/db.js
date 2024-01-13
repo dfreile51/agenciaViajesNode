b1da1dd24a75ea45600c36502b2feff8dbcef030
Diego Freile Garcia <dfreile51@gmail.com>
2024-01-13 13:04:02 +0100

    Añadido .env al proyecto

--- config/db.js
+++ config/db.js
@@ -1,9 +1,8 @@
 import Sequelize from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
-const db = new Sequelize("agenciaviajes", "root", "", {
-    host: "127.0.0.1",
-    port: "3306",
-    dialect: "mysql",
+const db = new Sequelize(process.env.DB_URL, {
     define: {
         timestamps: false,
     },
